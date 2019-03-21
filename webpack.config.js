9f21c1d0ebfad7ba6bb226e01b9c0296b9745dc1
Ruslan Chabin <pycc_lan@mail.ru>
2019-03-21 15:40:43 +0300

    webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,109 @@
+//basic
+const path = require('path');
+const webpack = require('webpack');
+//plugins
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	
+	entry: {
+		app: ['./js/index.js',
+			  './scss/style.scss',
+			  ],
+	},
+	
+	output: {
+		filename: 'js/main.js',
+		path: path.resolve(__dirname, './dist'),
+		//publicPath: '../'
+	},
+	
+	module: {
+		rules: [
+			{ 
+			  test: /\.pug$/,
+			  use: [{
+				  loader: 'pug-loader',
+			  	  options:{
+					  pretty: true
+				  },
+			  }],
+			},
+			{
+			test: /\.scss/,
+			use: ExtractTextPlugin.extract({
+				use: [
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+							url: false,
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						},
+					},
+				],
+				fallback: 'style-loader',
+			})
+			},
+			{
+				test: /\.(png|gif|jpe?g)$/,
+				loader: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+						},
+					},
+					'img-loader',
+				],
+			},
+			{
+				test: /\.(woff|woff2|ttf|eot|otf|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+						}
+					},
+				]
+			}
+		],
+	},
+	plugins: [
+		new ExtractTextPlugin('./css/style.css'),
+		
+		new CopyWebpackPlugin(
+			[
+				{from:'./images', to: './images'},
+				{from:'./fonts', to: './fonts'},
+			]),
+		new UglifyJSPlugin({
+			sourceMap: true
+		}),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true
+		}),
+		new HtmlWebpackPlugin({
+			template: './index.pug'
+		  }),
+	],
+};
+
