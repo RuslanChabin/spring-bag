dadddbf417d1c6ea03987dff295c15b45c56457b
Ruslan Chabin <pycc_lan@mail.ru>
2019-03-29 14:10:13 +0300

    map block

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,32 @@ var range1 = document.querySelector('.slider--green');
 
 range1.addEventListener('input', function () {   document.querySelector('.slider-output--green').style.width = 2.5*range1.value +'px'
 },false);
+
+//map
+
+ymaps.ready(init);
+ 
+function init(){     
+ 
+    var myMap;
+ 
+    myMap = new ymaps.Map("map", {
+        center: [37.787509, -122.444838],
+        zoom: 13,
+        controls: []
+    });
+ 
+    myMap.behaviors.disable('scrollZoom');
+ 
+    myMap.controls.add("zoomControl", {
+        position: {top: 15, left: 15}
+    });
+	
+	 var myPlacemark = new ymaps.Placemark([37.787509, -122.444838] , {},
+        { iconLayout: 'default#image',
+          iconImageHref: './images/marker.png',
+          iconImageSize: [59, 60],
+          iconImageOffset: [-15, -50] });     
+ 
+    myMap.geoObjects.add(myPlacemark);
+}
