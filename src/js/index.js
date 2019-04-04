13aa2202117434258ea8618268aa3ab90b4305c0
Ruslan Chabin <pycc_lan@mail.ru>
2019-04-04 12:02:31 +0300

    profile page

--- src/js/index.js
+++ src/js/index.js
@@ -82,23 +82,3 @@ document.querySelector('.calendar-button--left').onclick = function() {
 document.querySelector('.calendar-button--right').onclick = function() {
   Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month)+1);
 }
-
-//orange range input
-
-var range = document.querySelector('.slider--orange');
-
-document.querySelector('.slider-output').innerHTML = range.value,
-
-range.addEventListener('input', function (){
-  document.querySelector('.slider-output').innerHTML = range.value},false);
-
-range.addEventListener('input', function () {   document.querySelector('.slider-output').style.left = 2.33*range.value +'px'
-},false);
-
-//green range input
-
-var range1 = document.querySelector('.slider--green');
-
-range1.addEventListener('input', function () {   document.querySelector('.slider-output--green').style.width = 2.5*range1.value +'px'
-},false);
-
